91487297b6e3d538fc03a212b2f90c9c9339eec8
Iuto <lr170806@gmail.com>
2026-06-07 02:46:08 -0300

    Remoção de Procfile conflituoso e atualização das configurações de gunicorn.conf.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn lexabyte.wsgi:application --bind 0.0.0.0:$PORT
+web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn -c gunicorn.conf.py back.lexabyte.wsgi:application   
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,1 +1,17 @@
-pythonpath = 'back'
+import os
+import multiprocessing
+
+bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+keepalive = 5
+timeout = 30
+max_requests = 1000
+max_requests_jitter = 50
+preload_app = True
+daemon = False
+umask = 0
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
