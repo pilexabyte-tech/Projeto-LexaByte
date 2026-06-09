84b22a6eb1af5ea9c3fa8d01d197ca2e42a1ce7e
pilexabyte-tech <pi.lexabyte@gmail.com>
2026-06-09 20:56:28 -0300

    Atualização do Gunicorn

--- back/lexabyte/wsgi.py
+++ back/lexabyte/wsgi.py
@@ -1,6 +1,12 @@
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+BACKEND_DIR = Path(__file__).resolve().parent.parent
+if str(BACKEND_DIR) not in sys.path:
+	sys.path.insert(0, str(BACKEND_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lexabyte.settings')
 
